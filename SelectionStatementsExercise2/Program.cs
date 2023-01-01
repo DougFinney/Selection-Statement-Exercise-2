15febbc30b6bb62dfb520f36a713bd2dd25a9269
Doug Finney <dugfiny@gmail.com>
2023-01-01 06:31:19 -0600

    Completed Selection Statment Ex 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            String subject = Console.ReadLine();
+            {
+
+
+                switch (subject.ToLower())
+                {
+                    case "math":
+                    case "Math":
+                    case "mathmatics":
+                    case "Mathmatics":
+                        Console.WriteLine("Math is not my favorite subject!");
+                        break;
+                    case "science":
+                    case "Science":
+                        Console.WriteLine("Science is Cool!!");
+                        break;
+                    case "pe":
+                    case "PE":
+                    case "physical education":
+                    case "Physical Education":
+                    case "Physical education":
+                        Console.WriteLine("Everyone enjoys PE!!!");
+                        break;
+                    case "history":
+                        Console.WriteLine("History is also my favorite subject!!!!");
+                        break;
+                    case "english":
+                        Console.WriteLine("English papers can be fun to write!");
+                        break;
+                    default:
+                        Console.WriteLine("I do not know that subject...");
+                        break;
+                }
+
+            }
         }
+
     }
-}
+
+
+}   
